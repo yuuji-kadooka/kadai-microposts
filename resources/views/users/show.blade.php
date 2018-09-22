5658572ca22f37bed8320fca09315cad7fc04a02
yuji kadooka <YUJIXSHIFON@YAHOO.CO.JP>
2018-09-22 08:12:09 +0000

    post

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,16 +8,27 @@
                     <h3 class="panel-title">{{ $user->name }}</h3>
                 </div>
                 <div class="panel-body">
-                <img class="media-object img-responsive" src="{{ Gravatar::src($user->email, 500) }}"
+                    <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}"
                 </div>
             </div>
         </aside>
         <div class="col-xs-8">
             <ul class="nav nav-tabs nav-justified">
-                <li><a href="#">TimeLine</a></li>
+                <li role="presentation" class="{{ Request::is('user/' . $user->id) ? 'active' : '' }}"><a href="{{ route('users.show', ['id' => $user-id]) }}">TimeLine <span class="badge">{{ $count_microposts }}</span></a></li>
                 <li><a href="#">Followings</a></li>
                 <li><a href="#">Followers</a></li>
             </ul>
+            @if (Auth::id() == $user->id)
+                  {!! Form::open(['route' => 'microposts.store']) !!}
+                      <div class="form-group">
+                          {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
+                          {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
+                      </div>
+                  {!! Form::close() !!}
+            @endif
+            @if (count($microposts) > 0)
+                @include('microposts.microposts', ['microposts' => $microposts])
+            @endif
         </div>
     </div>
 @endsection
